6476fa1be35230be1d52ef27321c72e5680ba1d7
bellti9er <jongbeom0117@gmail.com>
2024-02-18 19:31:05 +0900

    Week1: CPU, Memory, Computer

---
+++ week1/cpu.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+class CPU(ABC):
+    @abstractmethod
+    def process(self, data):
+        pass
+
+class SingleCoreCPU(CPU):
+    def process(self, data):
+        return [data]
+    
+
+class DualCoreCPU(CPU):
+    def process(self, data):
+        return [data[::2], data[1::2]]
+
+class CPUFactory:
+    @staticmethod
+    def make_cpu(type):
+        if type == "single":
+            return SingleCoreCPU()
+        elif type == "dual":
+            return DualCoreCPU()
+        else:
+            raise ValueError("Invalid CPU type :", type)
+        
